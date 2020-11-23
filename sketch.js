7f2c7b9bef04f02bb34e13ff72159b074c1dd828
Marco Kade <kadem@htw-berlin.de>
2020-11-23 10:27:00 +0000

    added comment for introducing frameRate() for students working with older computer models

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 /* Alle Befehle für p5js findest du in der Dokumentation: https://p5js.org/reference/ */
 
 function setup() {
-  createCanvas(400, 400);
+	/* Für ältere Rechner könnt ihr hier während des Programmierens eine geringere Framerate einstellen. Damit benötigt p5js weniger Rechenleistung, zum Ausprobieren des visuellen Ergebnisses empfehle ich den Befehl wieder mittels der Zeichenfole "//" auszukommentieren.
+	
+	Per default liegt die Framerate bei 60 fps */
+
+	// frameRate(24);
+	
+	createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+	background(220);
 }
