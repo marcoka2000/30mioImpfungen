e4d751272478b4a94653bad459b3e212b7e92d3f
Marco Kade <kadem@htw-berlin.de>
2020-11-23 10:28:24 +0000

    fixed typo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 /* Alle Befehle für p5js findest du in der Dokumentation: https://p5js.org/reference/ */
 
 function setup() {
-	/* Für ältere Rechner könnt ihr hier während des Programmierens eine geringere Framerate einstellen. Damit benötigt p5js weniger Rechenleistung, zum Ausprobieren des visuellen Ergebnisses empfehle ich den Befehl wieder mittels der Zeichenfole "//" auszukommentieren.
+	/* Für ältere Rechner könnt ihr hier während des Programmierens eine geringere Framerate einstellen. Damit benötigt p5js weniger Rechenleistung, zum Ausprobieren des visuellen Ergebnisses empfehle ich den Befehl wieder mittels der Zeichenfolge "//" auszukommentieren.
 	
 	Per default liegt die Framerate bei 60 fps */
 
